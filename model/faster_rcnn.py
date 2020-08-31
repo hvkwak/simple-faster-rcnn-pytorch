71c4c37a42a4d518d98f6b611d1f98e02a5ea569
Hyovin Kwak <hyovin.kwak@tu-dortmund.de>
2020-08-31 12:25:53 +0200

    what

--- model/faster_rcnn.py
+++ model/faster_rcnn.py
@@ -1,5 +1,5 @@
-from __future__ import  absolute_import
-from __future__ import division
+# from __future__ import  absolute_import
+# from __future__ import division
 import torch as t
 import numpy as np
 from utils import array_tool as at
